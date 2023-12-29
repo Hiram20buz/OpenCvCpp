58a87a75d2d37430bcb356c93daee317e357b6c5
Hiram20buz <l20211847@tectijuana.edu.mx>
2023-12-29 11:54:08 -0800

    refactor

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Change directory to cppProject
+cd cppProject || exit
+
+# Create directory if it doesn't exist
+mkdir -p build
+
+# Change directory to the newly created 'build' directory
+cd build || exit
+
+# Run CMake to configure the build
+cmake ..
+
+# Use 'make' to compile the project
+make
+
+# Run the executable
+./MyProject
